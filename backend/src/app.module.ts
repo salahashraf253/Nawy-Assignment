9753b5db80eeaa406e54e8f5e1609693548c9026
Salah Ashraf <salahashraf924@gmail.com>
2025-04-21 15:01:52 +0200

    feat: configure TypeORM with Apartment entity and module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApartmentModule } from './apartment/apartment.module';
+import { Apartment } from './apartment/apartment.entity';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, 
+      entities: [Apartment],
+      synchronize: true, 
+      autoLoadEntities: true, 
+    }),
+    ApartmentModule,
+  ],
 })
 export class AppModule {}
