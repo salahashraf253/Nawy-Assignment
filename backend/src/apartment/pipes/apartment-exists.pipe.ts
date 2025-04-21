50844e69221e7f69400ce893fbadcad3660075c0
Salah Ashraf <salahashraf924@gmail.com>
2025-04-22 00:43:29 +0200

    chore: remove the logs

--- backend/src/apartment/pipes/apartment-exists.pipe.ts
+++ backend/src/apartment/pipes/apartment-exists.pipe.ts
@@ -13,8 +13,6 @@ import {
       const id = parseInt(value, 10);
       const apartment = await this.apartmentService.findOne(id);
       if (!apartment) {
-        console.error(`Apartment with ID ${id} not found`);
-        console.log('Apartment not found in the database');
         throw new NotFoundException(`Apartment with ID ${id} not found`);
       }
       return id;
