a9aa1405fe28c397e2d723b33ba81791dcf0592b
Salah Ashraf <salahashraf924@gmail.com>
2025-04-23 09:55:50 +0200

    feat: enhance apartment service with image handling

--- backend/src/apartment/apartment.service.ts
+++ backend/src/apartment/apartment.service.ts
@@ -4,18 +4,22 @@ import { Repository } from 'typeorm';
 import { Apartment } from './apartment.entity';
 import { CreateApartmentDto } from './dto/create-apartment.dto';
 import { FetchApartmentDto } from './dto/fetch-apartment.dto';
+import { Image } from '../image/image.entity';
 
 @Injectable()
 export class ApartmentService {
   constructor(
     @InjectRepository(Apartment)
-    private apartmentRepo: Repository<Apartment>,
+    private readonly apartmentRepo: Repository<Apartment>,
+    @InjectRepository(Image)
+    private readonly imageRepo: Repository<Image>,
   ) {}
 
   async findAll({ page, limit }: { page: number; limit: number }) {
     const [items, total] = await this.apartmentRepo.findAndCount({
       skip: (page - 1) * limit,
       take: limit,
+      relations: ['images'],
     });
 
     return {
@@ -28,11 +32,25 @@ export class ApartmentService {
   }
 
   async findOne(id: number): Promise<FetchApartmentDto> {
-    return await this.apartmentRepo.findOneBy({ id });
+    const apartment = await this.apartmentRepo.findOne({
+      where: { id },
+      relations: ['images'], 
+    });
+    if (!apartment) {
+      throw new NotFoundException(`Apartment with ID ${id} not found`);
+    }
+    return apartment;
   }
 
-  async create(dto: CreateApartmentDto): Promise<Apartment> {
+  async create(dto: CreateApartmentDto,images: Express.Multer.File[]): Promise<Apartment> {
     const apartment = this.apartmentRepo.create(dto);
+    apartment.images=[];
+    for (const image of images) {
+      const newImage = this.imageRepo.create({
+        url: `/uploads/${image.filename}`,
+      });
+      apartment.images.push(newImage);
+    }
     return await this.apartmentRepo.save(apartment);
   }
   async remove(id: number) {
