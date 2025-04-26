107722b819b037e5cefe1cc35183358e9f7321f9
Salah Ashraf <salahashraf924@gmail.com>
2025-04-26 13:16:10 +0300

    docs(backend): Swagger setup for apartments API

--- backend/src/apartment/apartment.controller.ts
+++ backend/src/apartment/apartment.controller.ts
@@ -3,12 +3,25 @@ import { ApartmentService } from './apartment.service';
 import { CreateApartmentDto } from './dto/create-apartment.dto';
 import { ApartmentExistsPipe } from './pipes/apartment-exists.pipe';
 import { FilesInterceptor } from '@nestjs/platform-express';
-
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FetchApartmentDto } from './dto/fetch-apartment.dto';
+@ApiTags('Apartments')
 @Controller('apartments')
 export class ApartmentController {
   constructor(private readonly apartmentService: ApartmentService) {}
 
   @Get()
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  @ApiOkResponse({ description: 'List of apartments' })
+
   async getAll(
     @Query('page') page?: string,
     @Query('limit') limit?: string,
@@ -20,11 +33,15 @@ export class ApartmentController {
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id', type: Number })
+  @ApiOkResponse({type: FetchApartmentDto})
   async getOne(@Param('id', ApartmentExistsPipe) id: number) {
     return await this.apartmentService.findOne(id);
   }
 
   @Post()
+  @ApiBody({ type: CreateApartmentDto })
+  @ApiCreatedResponse({ type: FetchApartmentDto })
   @UseInterceptors(FilesInterceptor('images', 10))
   async create(
     @Body() createApartmentDto,
@@ -34,6 +51,8 @@ export class ApartmentController {
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id', type: Number })
+  @ApiOkResponse({ description: 'Apartment deleted successfully' })
   async delete(@Param('id', ApartmentExistsPipe) id: number) {
     return await this.apartmentService.remove(id);
   }
